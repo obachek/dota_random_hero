d1adf46bdf59ad5eda72f2df7e3da41c7d3262d9
obachek <obachek@yandex.ru>
2016-01-11 17:54:01 +0300

    dota_random_hero 1-st version(withous all heroes and withouts items)

---
+++ dota_random_hero.rb
@@ -0,0 +1,3 @@
+heroes = ['abbadon', 'eartshaker', 'omniknight', 'timbersaw', 'axe', 'night stalker', 'sven', 'huskar', 'bristeback', 'pudge', 'DOOM', 'tiny', 'alchemist', 'tuskar', 'sand king', 'spirit breaker', 'kunnka', 'io', 'clockwerk', 'pheonix', 'shdow fiend', 'rubick', 'queen of pain', 'ancient apparition', 'puck', 'lion', 'witch doctor', 'dazzle', 'lina', 'ogrie magie']
+
+puts heroes.sample
